3a8d0405f6f1dfee0ffd73ef0fa81e4d18402f9a
Johnny Machado <johnnymachado490@gmail.com>
2021-12-22 18:31:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-const world_width = 100
-const world_height = 30
+import {updateGround} from './ground'
 
-const worldElem = document.querySelector(['data-world']);
+const WORLD_WIDTH = 100
+const WORLD_HEIGHT = 30
 
-pixelWorldScale()
-window.addEventListener("resize", pixelWorldScale) 
+const worldElem = document.querySelector("[data-world]");
 
-function pixelWorldScale() {
+setPixelWorldScale()
+window.addEventListener("resize", setPixelWorldScale) 
+
+let lastTime
+function update (time) {
+    if (lastTime == null) {
+        lastTime = time
+        window.requestAnimationFrame(update)
+        return
+    }
+    const delta = time - lastTime
+
+    updateGround(delta)
+
+    lastTime = time
+    window.requestAnimationFrame(update)
+}
+window.requestAnimationFrame(update)
+
+function setPixelWorldScale() {
     let worldPixelScale
-    if (window.innerWidth / window.innerHeight < world_width / world_height) 
-    {
-        worldPixelScale = window.innerWidth / world_width
-    } else 
-    {
-        worldPixelScale = window.innerHeight / world_height
+    if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
+        worldPixelScale = window.innerWidth / WORLD_WIDTH
+    } else {
+        worldPixelScale = window.innerHeight / WORLD_HEIGHT
     }
 
-    worldElem.style.width = `${world_width * worldPixelScale}px`
-    worldElem.style.height = `${world_height * worldPixelScale}px`
+    worldElem.style.width = `${WORLD_WIDTH * worldPixelScale}px`
+    worldElem.style.height = `${WORLD_HEIGHT * worldPixelScale}px`
 }
